45323825302e5c5be77e8fe632c07fa66415b54a
= <=>
2017-09-22 21:50:47 +0700

    commit to repository

---
+++ DongVat.java
@@ -0,0 +1,7 @@
+	public class DongVat {
+		public static void main(String[] args) {
+			System.out.println("Dong Vat rat da dang") ;
+			System.out.println("song o tren can") ;
+			System.out.println("song o duoi nuoc") ;
+	}
+}
